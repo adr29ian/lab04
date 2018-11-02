82610059233fb5accf49f6af0b24a4e1fcb7e4f3
adr29ian <adriansingh29@gmail.com>
2018-11-02 20:52:05 +0000

    final

--- irtester.c
+++ irtester.c
@@ -3,16 +3,37 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
   while(1) {
-    printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
-    printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
-    printf("Alarm\n");
-  }
-  /*NOTREACHED*/
-  return 0 ;
+    printf("Waiting for reset \n");
+    while(digitalRead(0) ==1)
+{
+wiringPiSetup() ;
+pinMode (2,OUTPUT);
+
+digitalWrite (2,HIGH); delay(250);
+digitalWrite(2,LOW); delay(250);
+	printf("ONE \n");
+ifttt(/*fill in */);
+
+}
+
+printf("Waiting for event \n");
+while(digitalRead(0) ==0)
+{
+wiringPiSetup();
+pinMode(1, OUTPUT);
+
+digitalWrite(1,HIGH); delay(250);
+digitalWrite(1,LOW); delay(250);
+printf("ZERO \n");
+
+ifttt(/*fill in*/);
+}
+
+printf("Alarm \n");
+}
+/*NOT REACHED*/
+return 0;
 }
